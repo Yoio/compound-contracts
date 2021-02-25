6a675bda3c2afd535857fd7c91abcd8cf9376675
williamliu <geek.yoio.liu@gmail.com>
2021-02-25 17:40:59 +0800

    init add

---
+++ contracts/oracle.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.5.16;
+interface CToken{
+    function symbol() external view returns (string memory);
+    function underlying() external view returns(address);
+}
+contract PriceOracle {
+    /// @notice Indicator that this is a PriceOracle contract (for inspection)
+    bool public constant isPriceOracle = true;
+
+    /**
+      * @notice Get the underlying price of a cToken asset
+      * @param cToken The cToken to get the underlying price of
+      * @return The underlying asset price mantissa (scaled by 1e18).
+      *  Zero means the price is unavailable.
+      */
+    function getUnderlyingPrice(CToken cToken) external view returns (uint);
+}
+
+
+contract SimplePriceOracle is PriceOracle {
+    mapping(address => uint) prices;
+    event PricePosted(address asset, uint previousPriceMantissa, uint requestedPriceMantissa, uint newPriceMantissa);
+
+    function getUnderlyingPrice(CToken cToken) public view returns (uint) {
+        if (compareStrings(cToken.symbol(), "Compound OKT")) {
+            return 1e18;
+        } else {
+            return prices[address(CToken(address(cToken)).underlying())];
+        }
+    }
+
+    function setUnderlyingPrice(CToken cToken, uint underlyingPriceMantissa) public {
+        address asset = address(CToken(address(cToken)).underlying());
+        emit PricePosted(asset, prices[asset], underlyingPriceMantissa, underlyingPriceMantissa);
+        prices[asset] = underlyingPriceMantissa;
+    }
+
+    function setDirectPrice(address asset, uint price) public {
+        emit PricePosted(asset, prices[asset], price, price);
+        prices[asset] = price;
+    }
+
+    // v1 price oracle interface for use as backing of proxy
+    function assetPrices(address asset) external view returns (uint) {
+        return prices[asset];
+    }
+
+    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+}
